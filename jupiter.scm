517f2ba9be157a92c539db90b09369a95189ced5
Jack Mousseau <jackmousseau@gmail.com>
2016-02-06 14:13:45 -0500

    Add cond for statements in interpret-parse-tree

--- jupiter.scm
+++ jupiter.scm
@@ -2,10 +2,18 @@
 ; Case Western Reserve PLC Spring 2016
 
 (load "simple-parser.scm")
+(load "stmt-conds.scm")
 
 (define interpret
     (lambda (filename)
         (interpret-parse-tree (parser filename) '(()()))))
 
 (define interpret-parse-tree
-    (lambda (parse-tree state) (error 'missing "interpret-parse-tree not implemented"))
+    (lambda (parse-tree state)
+        (cond
+            ((var-declaration-stmt? (car parse-tree)) 'unimplemented)
+            ((assigment-stmt? (car parse-tree)) 'unimplemented)
+            ((if-stmt? (car parse-tree)) 'unimplemented)
+            ((while-stmt? (car parse-tree)) 'unimplemented)
+            ((return-stmt? (car parse-tree)) 'unimplemented)
+            (else (error 'interpret-parse-tree "unrecognized branch in parse tree")))))
