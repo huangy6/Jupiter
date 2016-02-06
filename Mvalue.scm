f9078839ab8c367738249510fea7137508c04618
Jack Mousseau <jackmousseau@gmail.com>
2016-02-06 14:20:51 -0500

    Add Mvalue_expression for non-var expressions

--- Mvalue.scm
+++ Mvalue.scm
@@ -1,2 +1,33 @@
 ; Created by Jack Mousseau, Vimig Socrates, and Yidi Huang
 ; Case Western Reserve PLC Spring 2016
+
+(define Mvalue_expression
+    (lambda (expression state)
+        (cond
+            ((number? expression) expression)
+            ((eq? 'true expression) #t)
+            ((eq? 'false expression) #f)
+            ; mathematical operators
+            ((eq? (operator expression) '+) (+ (Mvalue_expression (left-operand expression) state) (Mvalue_expression (right-operand expression) state)))
+            ((and (eq? (operator expression) '-) (null? (cddr expression))) (- (Mvalue_expression (left-operand expression) state)))
+            ((eq? (operator expression) '-) (- (Mvalue_expression (left-operand expression) state) (Mvalue_expression (right-operand expression) state)))
+            ((eq? (operator expression) '*) (* (Mvalue_expression (left-operand expression) state) (Mvalue_expression (right-operand expression) state)))
+            ((eq? (operator expression) '/) (quotient (Mvalue_expression (left-operand expression) state) (Mvalue_expression (right-operand expression) state)))
+            ((eq? (operator expression) '%) (remainder (Mvalue_expression (left-operand expression) state) (Mvalue_expression (right-operand expression) state)))
+            ; comparision operators
+            ((eq? (operator expression) '==) (equal? (Mvalue_expression (left-operand expression) state) (Mvalue_expression (right-operand expression) state)))
+            ((eq? (operator expression) '!=) (not (equal? (Mvalue_expression (left-operand expression) state) (Mvalue_expression (right-operand expression) state))))
+            ((eq? (operator expression) '<) (< (Mvalue_expression (left-operand expression) state) (Mvalue_expression (right-operand expression) state)))
+            ((eq? (operator expression) '>) (> (Mvalue_expression (left-operand expression) state) (Mvalue_expression (right-operand expression) state)))
+            ((eq? (operator expression) '<=) (<= (Mvalue_expression (left-operand expression) state) (Mvalue_expression (right-operand expression) state)))
+            ((eq? (operator expression) '>=) (>= (Mvalue_expression (left-operand expression) state) (Mvalue_expression (right-operand expression) state)))
+            ; boolean operators
+            ((eq? (operator expression) '&&) (and (Mvalue_expression (left-operand expression)) (Mvalue_expression (right-operand expression) state)))
+            ((eq? (operator expression) '||) (or (Mvalue_expression (left-operand expression)) (Mvalue_expression (right-operand expression) state)))
+            ((eq? (operator expression) '!) (not (Mvalue_expression expression state)))
+
+            (else (error 'unknown "unkown expression")))))
+
+(define operator car)
+(define left-operand cadr)
+(define right-operand caddr)
