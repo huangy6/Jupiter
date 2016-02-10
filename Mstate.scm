d30f2f9302b1ec7a72a110037a851e3dd6f13fb1
Jack Mousseau <jackmousseau@gmail.com>
2016-02-10 17:00:08 -0500

    Move Mstate to Mstate.scm

--- Mstate.scm
+++ Mstate.scm
@@ -7,6 +7,22 @@
 ;                                   Core
 ; =============================================================================
 
+(define Mstate
+    (lambda (parse-tree state)
+        (cond
+            ((var-declaration-stmt? (branch parse-tree))
+                (if (null? (cddar parse-tree))
+                    (interpret-parse-tree (cdr parse-tree) (Mstate_var-declaration-stmt (first-param parse-tree) state))
+                    (interpret-parse-tree (cdr parse-tree) (Mstate_var-declaration-stmt-with-value (first-param parse-tree) (second-param parse-tree) state))))
+            ((assigment-stmt? (branch parse-tree)) (interpret-parse-tree (cdr parse-tree) (Mstate_assignment-stmt (first-param parse-tree) (second-param parse-tree) state)))
+            ((if-stmt? (branch parse-tree))
+                (if (null? (cdddar parse-tree))
+                    (interpret-parse-tree (cdr parse-tree) (Mstate_if-stmt (first-param parse-tree) (second-param parse-tree) state))
+                    (interpret-parse-tree (cdr parse-tree) (Mstate_if-else-stmt (first-param parse-tree) (second-param parse-tree) (third-param parse-tree) state))))
+            ((while-stmt? (branch parse-tree)) 'unimplemented)
+            ((return-stmt? (branch parse-tree)) (Mstate_return-stmt (first-param parse-tree) state))
+            (else (error 'interpret-parse-tree "unrecognized branch in parse tree")))))
+
 ; insert the 'return var into the state
 (define Mstate_return-stmt
     (lambda (return-stmt state)
--- jupiter.scm
+++ jupiter.scm
@@ -10,22 +10,6 @@
     (lambda (filename)
         (Mvalue_return (Mstate (parser filename) '(()())))))
 
-(define Mstate
-    (lambda (parse-tree state)
-        (cond
-            ((var-declaration-stmt? (branch parse-tree))
-                (if (null? (cddar parse-tree))
-                    (interpret-parse-tree (cdr parse-tree) (Mstate_var-declaration-stmt (first-param parse-tree) state))
-                    (interpret-parse-tree (cdr parse-tree) (Mstate_var-declaration-stmt-with-value (first-param parse-tree) (second-param parse-tree) state))))
-            ((assigment-stmt? (branch parse-tree)) (interpret-parse-tree (cdr parse-tree) (Mstate_assignment-stmt (first-param parse-tree) (second-param parse-tree) state)))
-            ((if-stmt? (branch parse-tree))
-                (if (null? (cdddar parse-tree))
-                    (interpret-parse-tree (cdr parse-tree) (Mstate_if-stmt (first-param parse-tree) (second-param parse-tree) state))
-                    (interpret-parse-tree (cdr parse-tree) (Mstate_if-else-stmt (first-param parse-tree) (second-param parse-tree) (third-param parse-tree) state))))
-            ((while-stmt? (branch parse-tree)) 'unimplemented)
-            ((return-stmt? (branch parse-tree)) (Mstate_return-stmt (first-param parse-tree) state))
-            (else (error 'interpret-parse-tree "unrecognized branch in parse tree")))))
-
 (define branch car)
 (define first-param cadar)
 (define second-param caddar)
