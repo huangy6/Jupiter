1797e6665081fd63de16b59b564f72826de66df6
Jack Mousseau <jackmousseau@gmail.com>
2016-04-12 19:58:37 -0400

    Add class-parser for part 4

--- jupiter.scm
+++ jupiter.scm
@@ -1,20 +1,17 @@
 ;; Created by Jack Mousseau, Vimig Socrates, and Yidi Huang
 ;; Case Western Reserve PLC Spring 2016
 
-(load "functionParser.scm")
-;;(load "simple-parser.scm")
+(load "class-parser.scm")
 
 (load "Mvalue.scm")
 (load "Mstate.scm")
-
-;(define call/cc call-with-current-continuation)
 
 (define init-state (list init-layer))
 
 (define main-invocation '(funcall main))
 
 (define interpret
-  (lambda (filename)
+  (lambda (filename classname)
     ((lambda (state)
       ((lookup-var 'main state) '() state))
     (Mstate (parser filename) init-state init-gotos))))
