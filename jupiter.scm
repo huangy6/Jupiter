65d85da09374157cc55a4bb1bf017ff31768c7ee
huangy6 <huangy6@yahoo.com>
2016-04-21 16:24:05 -0400

    Use p3 parser

--- jupiter.scm
+++ jupiter.scm
@@ -1,7 +1,7 @@
 ;; Created by Jack Mousseau, Vimig Socrates, and Yidi Huang
 ;; Case Western Reserve PLC Spring 2016
 
-(load "class-parser.scm")
+(load "functionParser.scm")
 
 (load "Mvalue.scm")
 (load "Mstate.scm")
