442ae971c8bdc5e70e5bc15b8afe231e03350305
Jack Mousseau <jackmousseau@gmail.com>
2016-04-23 16:28:51 -0400

    Pass pretest 00

--- jupiter.scm
+++ jupiter.scm
@@ -10,8 +10,11 @@
 (define init-state (list init-layer))
 
 (define main-invocation '(funcall main))
+(define no-actual-params '())
 
 (define interpret
   (lambda (filename classname)
-    (initialize_classes (parser filename) init-state)))
+    ((lambda (state)
+       ((lookup-var 'main (list (get_method-layer (lookup-class (string->symbol classname) (get_class-layer state))))) no-actual-params state))
+       (initialize_classes (parser filename) init-state))))
 
