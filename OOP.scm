1bb705bfebf234be77bdcc0e15f7dccd5cc03566
Jack Mousseau <jackmousseau@gmail.com>
2016-04-23 12:13:42 -0400

    Add class layers

--- OOP.scm
+++ OOP.scm
@@ -4,7 +4,12 @@
 ;; =============================================================================
 ;;  classes
 ;; =============================================================================
+;;
+;; Complete state structure:
 ;; ((layer) (layer) ((A B) (a-class b-class))))
+;;
+;; Class structure:
+;; ('class parent-class property-layer method-layer) 
 
 (define lookup-class
   (lambda (class-name class-layer)
@@ -19,26 +24,47 @@
 (define no-parent-class 'null)
 
 (define new-class
-  (lambda (parent-class class-name)
+  (lambda (parent-class property-layer method-layer)
     (if (eq parent-class no-parent-class)
-        (list 'class class-name parent-class '() '())
-        (list 'class class-name parent-class (get_instance-fields parent-class) (get_methods parent-class)))))
+        (list 'class parent-class property-layer method-layer)
+        (list 'class parent-class
+              (clayer_merge property-layer (get_property-layer parent-class))
+              (clayer_merge method-layer (get_method-layer parent-class))))))
 
-(define get_class-name cadr)
-(define get_parent-class caddr)
-(define get_instance-fields cadddr)
-(define get_methods
-  (lambda (list)
-    (car (cddddr))))
+(define get_parent-class cadr)
+(define get_property-layer caddr)
+(define get_method-layer cadddr)
 
-(define set_instance-fields
-  (lambda (class instance-fields)
-    (set_at-index class-instance-fields-index instance-fields class)))
+;; =============================================================================
+;;  class layers (clayers)
+;; =============================================================================
 
-(define set_methods
-  (lambda (class methods)
-    (set_at-index class-methods-index methods class)))
+;; (clayer_merge '((a b) (1 2)) '((c d) (3 4))) => (((a b) c d) ((1 2) 3 4))
+(define clayer_merge
+  (lambda (layer-a layer-b)
+    (list (cons (car layer-a) (car layer-b)) (cons (cadr layer-a) (cadr layer-b)))))
 
+;; returns (default-value indices-remaining)
+;; NOTE: indices start at 0!
+(define clayer_search
+  (lambda (clayer)
+    (list
+     (layer_lookup-var (list (flatten (car clayer)) (flatten (cadr clayer))))
+     (indices-remainig (flatten (car clayer))))))
+
+(define clayer_super-search
+  (lambda (layer)
+    (if (list? (car layer))
+        (clayer_search (cdr layer))
+        (error 'clayer_super-search "no super class"))))
+
+(define indices-remaining
+  (lambda (l a)
+    (cond
+      ((null? (cdr l)) (error 'indices-remaining "cound not find variable")) 
+      ((eq? a (car l)) (length (cdr l)))
+      (else (indices-remaining (cdr l))))))
+    
 ;; =============================================================================
 ;;  instances
 ;; =============================================================================
@@ -62,11 +88,15 @@
 ;;  extras
 ;; =============================================================================
 
-(define class-instance-fields-index 3)
-(define class-methods-index 4)
-
 (define instance-type-index 1)
-(define instance-field-values-index 2) 
+(define instance-field-values-index 2)
+
+(define flatten
+  (lambda (l)
+    (cond
+      ((null? l) '())
+      ((list? (car l)) (append (flatten (car l)) (flatten (cdr l))))
+      (else (cons (car l) (flatten (cdr l)))))))
 
 (define set_at-index
     (lambda (index value list)
